d3280419c04bba6b7fc036316bae6adbf2d3886f
brian-p-nguyen <92738318+brian-p-nguyen@users.noreply.github.com>
2022-01-25 12:06:28 -0500

    fix cart compilation errors

--- components/cart/Cart.vue
+++ components/cart/Cart.vue
@@ -85,7 +85,8 @@ export default {
         //     updateCheckoutData(checkout);
         //   }
         // });
-    };
+      }
+    });
 
     const processCheckout = () => {
       const body = {
